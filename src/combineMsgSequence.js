a1b5986d60bab1212aca0ac7d53ae23e8f0f3fb4
huyen-nguyen <22818938+huyen-nguyen@users.noreply.github.com>
2021-11-05 04:04:06 -0500

    run ok: sorted ok + reformat + add header

--- src/combineMsgSequence.js
+++ src/combineMsgSequence.js
@@ -3,13 +3,15 @@ let fileName = "FullServiceCheckoutBase.tsv";
 loadData(fileName);
 
 function loadData(fileName){
-    d3.tsv("data/" + fileName, function (err, _data){
+    d3.tsv("data/" + fileName, function (err, data){
         if (err) throw err;
         else {
+			d3.select("body").append("h2")
+				.attr("id", "header")
             d3.select("body").append("div")
-                .attr("id", "msgSequence" );
+                .attr("id", "main" );
 
-            print(_data, "#msgSequence")
+            print(fileName, data)
         }
     })
 }
@@ -18,24 +20,22 @@ function combine(data){
     let array = [];
 
     data.forEach(d => {
+    	// separate different messages by |
         d.Content = d.Content.replace(/[A-Z][0-9]/g, m => "|" + m);
-        console.log(d)
 
-        // if content has |
+        // if content has multiple messages separated by |
         d.Content.split("|").filter(m => m.length > 0).forEach(msg => {
-            console.log(msg)
-
-            let order = msg.split(":")[0];
+        	let order = msg.split(":")[0];
             let content = msg.split(":")[1];
             let condition = "";
 
-            if (order.includes("[")){
+            if (order.includes("[")){		// contain condition
                 order = order.split("[")[0];
                 condition = " (if " + msg.split(":")[0].split("[")[1].split("]")[0] + ") ";
             }
 
-            array.push({
-                order: order.trim(),
+            array.push({	// array of all individual messages
+                order: order.trim().replace("F", "L"),
                 condition: condition.trim(),
                 content: content.trim(),
                 From: d.From,
@@ -44,43 +44,37 @@ function combine(data){
         })
     })
 
-
-    return array.sort((first, second) => {
-        let a = first.order.replaceAll(".", "");
-        let b = second.order.replaceAll(".", "");
-        if (a > b) {
-            if (a.includes(b)){
-                return -1;
-            }
-            return 1;
-        }
-        if (a < b) {
-            return -1;
-        }
-        return 0;
-    })
+	// sort by sequence order number
+	array.sort((a, b) => {
+		if (a.order > b.order) {
+			return 1;
+		}
+		if (a.order < b.order) {
+			return -1;
+		}
+		return 0;
+	})
+	return array;
 }
 
-function print(data, div){
+function print(fileName, data){
     let array = combine(data);
-
     let bigString = "";
 
     array.forEach(msg => {
+    	let c = desc(msg.content);
         bigString += '<span class="seqNumber">' + msg.order + ': </span>' +
-            '<span class="description">' + msg.condition +'</span>' +
+            '<span class="description">' + msg.condition +' </span>' +
             '<span class="object">' + processAlg(msg.From) +'</span>' +
-            '<span class="description">' + desc(msg.content).verb + '</span>' +
+            '<span class="description"> ' + c.verb + ' </span>' +
             '<span class="object">' + processAlg(msg.To) +'</span>' +
-            '<span class="description"> ' + desc(msg.content).content +  '</span>' +
+            '<span class="description"> ' + c.content +  '</span>' +
             '<br><br>';
     })
 
-    d3.select(div).html(bigString)
-
-
-
-
+	let s = "FullServiceCheckoutBase.tsv"
+	d3.select("#header").html(fileName.split(".")[0].replace(/[A-Z]/g, m => " " + m) + " Message Sequence Description");
+    d3.select("#main").html(bigString)
 }
 
 function processAlg(string){
@@ -91,19 +85,29 @@ function processAlg(string){
 }
 
 function desc(content){
-    let verbArray = ["retrieve", "open", "print", "display", "compute", "update", "request", "insert"]
+	let c = content.toLowerCase();
+	let primaryVerb = ["prompt to", "prompt for"]
+    let secondaryVerb = ["retrieve ", "open ", "print ", "display ", "compute ", "update ", "request ", "insert ", "calculate ", "scale ", "store "]
 
-    // // if there exists a verb above
-    // if (verbArray.filter(d => content.includes(d)).length > 0)
-    // {
-    //     return {
-    //         verb: " informs ",
-    //         content: " to " + content.toLocaleLowerCase()
-    //     }
-    // }
+    // if there exists a verb above
+    if (secondaryVerb.some(d => c.includes(d))) {
+        return {
+            verb: "informs",
+            content: "to " + c
+        }
+    }
+
+    let pVerb = primaryVerb.find(d => c.includes(d))
+    if (pVerb){
+    	let main = pVerb.split(" ")[0];
+    	return {
+    		verb: main + "s",
+			content: c.replace(main, "")
+		}
+	}
 
     return {
-        verb: " sends to ",
-        content: content.toLocaleLowerCase()
+        verb: "sends to ",
+        content: c
     }
 }
